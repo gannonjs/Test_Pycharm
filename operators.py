f224d22eaabd3ca694cd92817b2a49ee15771350
gannonjs <jonah.gannon@gmail.com>
2019-01-14 21:10:51 +0800

    messing about with basic operators

---
+++ operators.py
@@ -0,0 +1,25 @@
+number = 1 + 2 * 3 / 4.0
+
+print("The Number is: %d" % number)
+
+remainder = 11 % 3
+
+print("Percentage is a modulo operator: %d" % remainder)
+
+squared = 2 ** 2
+
+cubed = 2**3
+
+print("A number squared is **2 and cubed is **3:", squared, cubed)
+
+helloworld = "hello" + " " + "world"
+
+print(helloworld)
+
+lotsofhellos = "hello " * 10
+
+F_lotsofhellos = "hello " * 2 **4
+
+print(lotsofhellos)
+
+print(F_lotsofhellos)
