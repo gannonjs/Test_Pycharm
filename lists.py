f2a6844115c38b50df83df25c9518567975b15e9
gannonjs <jonah.gannon@gmail.com>
2019-01-14 20:53:13 +0800

    lists being tested

---
+++ lists.py
@@ -0,0 +1,11 @@
+mylist = []
+
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+
+print("The first entry is", mylist[0])
+print("The second entry is", mylist[1])
+print("The third entry is", mylist[2])
+
+print("So the list is:", mylist)
