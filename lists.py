b526dcfea4fc373f26366fc2d2fdcad390bdb06e
gannonjs <jonah.gannon@gmail.com>
2019-01-14 20:59:08 +0800

    added the test from the tutorial

--- lists.py
+++ lists.py
@@ -9,3 +9,21 @@ print("The second entry is", mylist[1])
 print("The third entry is", mylist[2])
 
 print("So the list is:", mylist)
+
+
+
+print("###############TEST BELOW###########")
+
+numbers = []
+strings = []
+
+names = ["John", "Eric", "Jessica"]
+
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+
+strings = names
+
+print(numbers)
+print(strings)
